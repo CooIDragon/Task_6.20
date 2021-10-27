49f1fc82eb60ba09037be61e3ca3c236569a6b35
CooIDragon <90614965+CooIDragon@users.noreply.github.com>
2021-10-27 15:03:16 +0300

    Update Main.java

--- src/ru/vsu/cs/baturin_v_a/Main.java
+++ src/ru/vsu/cs/baturin_v_a/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        
         Locale.setDefault(Locale.ROOT);
 
         System.out.print("Введите x = ");
